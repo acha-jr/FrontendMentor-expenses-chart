54255f6ef54626ada6e8f67d386eeed2d60b463c
Obinna Achazie <105124616+acha-jr@users.noreply.github.com>
2022-06-03 14:49:53 +0100

    Chart Expenses Upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const spent = [...document.querySelectorAll("span")];
+const bars = [...document.querySelectorAll(".bar")];
+const days = [...document.querySelectorAll(".day p")];
+
+fetch("./data.json")
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    spent.forEach((e) => {
+      let current = spent.indexOf(e);
+      spent[current].textContent = "$" + data[current].amount;
+      days[current].textContent = data[current].day;
+      bars[current].style.height = `${data[current].amount * 3}px`;
+    });
+  });
+
+// Sets background for current day
+let today = new Date().getDay();
+today = today - 1;
+
+for (i = 0; i < bars.length; i++) {
+  bars[i].classList.remove("today");
+}
+bars[today].classList.add("today");
